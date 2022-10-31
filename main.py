85adc7d867e0550bfbe50df49a5c440671d14a54
MUKESH BABU <108213299+GOD-OF-LOVE@users.noreply.github.com>
2022-10-31 22:31:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 import re
 
 
-API_ID = os.environ.get("API_ID", None)) 
+API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None))
 KUKI_API = os.environ.get("KUKI_API", None) 
